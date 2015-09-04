e908c8529e8f2ca005a673618524f7c2ef7f6c19
Andrew Schmidt <drew.schmidt@me.com>
2015-09-04 01:55:54 -0700

    Transitioned to PostgreSQL!
    This is insanely exciting. My. First. Database!
    
    I used Peewee, a Postgres wrapper for Python that lets me build the
    data model out of regular-ish Python classes. It’s great.
    
    And while I could’ve gone with the built in SQLite, I decided on
    Postgres because it’ll be great for transitioning to a web app,
    eventually. Right now I’m planning on using Flask for that, because
    it’s lightweight & lets me bring my own database.
    
    This is probably a good stopping point for tonight.

--- hiuni_data.py
+++ hiuni_data.py
@@ -1,18 +1,29 @@
 # The data model, and classes related to managing data in and out.
 
+from peewee import *
 
-class School:
-	def __init__(self, name, IPEDS_id, majors):
-		self.name = name
-		self.IPEDS_id = IPEDS_id
-		self.majors = majors
+database = PostgresqlDatabase("hiuni_database", user = "Andrew")
 
 
-class Major:
-	def __init__(self, name, cip, median_salary):
-		self.name = name
-		self.cip = cip
-		self.median_salary = median_salary
+
+class BaseModel(Model):
+	class Meta:
+		database = database
+
+
+class School(BaseModel):
+	name = CharField()
+	ipeds_id = IntegerField()
+
+	def majors(self):
+		return Major.select().join(School).where(School == self) #Or something.
+
+
+class Major(BaseModel):
+	school = ForeignKeyField(School, related_name = "majors")
+	name = CharField()
+	cip = FloatField()
+	median_salary = IntegerField()
 
 
 
@@ -22,6 +33,15 @@ class data_helper:
 	
 	def __init__(self):
 		return
+
+
+	def get_sheet(self):
+		# Load the workbook & sheet:
+		book = xlrd.open_workbook("PayScale Sample (altered).xlsx")
+		sheet = book.sheet_by_name("4-Digit CIP - Experienced Pay")
+
+		return sheet
+
 
 	def get_school_names_from_sheet(self, sheet):
 		school_names = []
@@ -51,7 +71,7 @@ class data_helper:
 		return school_ids
 
 
-	def get_majors_for_school_with_id(self, school_id, from_sheet):
+	def create_majors_for_school(self, school, from_sheet):
 		majors = []
 
 		sheet = from_sheet
@@ -60,31 +80,58 @@ class data_helper:
 		for row in range(number_of_rows):
 			row_id = sheet.cell_value(rowx = row, colx = 0)
 			major_name = sheet.cell_value(rowx = row, colx = 2)
-			cip_code = sheet.cell_value(rowx = row, colx = 3)
+			cip = sheet.cell_value(rowx = row, colx = 3)
 			median_salary = sheet.cell_value(rowx = row, colx = 5)
 
-			if row_id == school_id:
-				major = Major(name = major_name, cip = cip_code, median_salary = median_salary)
-				majors.append(major)
-
-		return majors
+			if row_id == school.ipeds_id:
+				major = Major.create(school = school, name = major_name, cip = cip, median_salary = median_salary)
 
 
-	def load_schools_from_xls(self, xls_name):
-		# Load the workbook & sheet:
-		book = xlrd.open_workbook(xls_name)
-		sheet = book.sheet_by_name("4-Digit CIP - Experienced Pay")
-
+	def create_schools_from_sheet(self, sheet):
 		# First, let's get basic info about the schools.
 		school_names = self.get_school_names_from_sheet(sheet)
 		school_ids = self.get_school_ids_from_sheet(sheet)
 
-		# Next, let's get each school's majors as an array of Major objects, and we'll make School objects in the process.
+		# Let's add some schools to the database, but only if they aren't already there!
 		schools = []
+		existing_schools = School.select()
 
-		for i in range(len(school_ids)):
-			majors = self.get_majors_for_school_with_id(school_ids[i], from_sheet = sheet)
-			school = School(name = school_names[i], IPEDS_id = school_ids[i], majors = majors)
-			schools.append(school)
+		for i in range(len(school_names)):
+			try:
+				School.get(School.name == school_names[i])
+
+			except Exception:
+				print "\nLooks like the", school_names[i], "isn't in the database yet. Adding it!"
+				school = School.create(name = school_names[i], ipeds_id = school_ids[i])
+				schools.append(school)
+
+			# else:
+			# 	print "Duplicate detected!"
 
 		return schools
+
+
+	def load_schools_and_majors(self):
+		# First load the spreadsheet:
+		sheet = self.get_sheet()
+		
+		# Next create the schools:
+		schools = self.create_schools_from_sheet(sheet)
+
+		# Finally create all the majors for each school:
+		for school in schools:
+			self.create_majors_for_school(school, from_sheet = sheet)
+
+
+	def create_database(self):
+		# Only run this once.
+		database.connect()
+		# database.create_tables([School, Major]) #Seriously, just once!
+		self.load_schools_and_majors()
+
+
+	def delete_schools(self):
+		# Truncate, meaning, delete all data.
+		schools = School.select()
+		for school in schools:
+			school.delete_instance(recursive = True)
