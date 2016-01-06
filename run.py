7fb7395905cc0769f149c4fcf4b8f531eda7a655
Andrew Schmidt <scatterfelt@gmail.com>
2016-01-05 18:52:13 -0800

    Moved all the code so far into the "app/" folder. I may bury it all even deeper, but that’s good for now.
    And I got started building the Flask app: nothing but a “Hello world”
    at the index right now, but I’m really just trying to learn how web
    apps are structured.

---
+++ run.py
@@ -0,0 +1,7 @@
+#!/Users/Andrew/.virtualenvs/hiuni/bin/python
+
+# Change the above path to the correct python for whatever system this is installed on.
+
+from app import app
+
+app.run(debug = True)
