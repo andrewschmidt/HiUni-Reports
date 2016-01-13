eb95c806e396470a9ea2a3f4dd0563e8d316698c
Andrew Schmidt <scatterfelt@gmail.com>
2016-01-12 21:52:05 -0800

    Fixed validator error presentation, changed State field to a drop down
    Like the title says.
    
    I also removed some commented-out code from when I was using WTForms
    built-in SelectField for choosing a career, instead of wtf-peewee’s
    SelectQueryField.
    
    Sure, with this approach I have to import an extra module, but I think
    it’s worth it to have a drop down field that can return actual career
    objects instead of just strings.

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from playhouse.postgres_ext import *
 from flask.ext.wtf import Form
 from wtforms.fields import StringField, SelectField
 from wtforms.fields.html5 import EmailField
-from wtforms.validators import DataRequired, Email, Required
+from wtforms.validators import Email, Required
 
 from wtfpeewee.fields import SelectQueryField # Unlike a regular SelectField, this returns actual model classes.
 from wtfpeewee.orm import model_form # For modeling forms from database objects. Not sure I really need it.
@@ -52,32 +52,29 @@ def after_request(response):
 # FORMS
 
 class Questionnaire_Form(Form):
-	first_name = StringField("First name:")
-	last_name = StringField("Last name:")
-
-	email = StringField("Email:", validators = [Required("Please enter your email."), Email("Please enter an email address.")])
-
-	# careers = Career.select()
-	# career_choices = [(None, "")]
-	# for career in careers:
-	# 	career_choices.append((career.name, career.name))
-	# career = SelectField("Career:", choices = [])
-
-	career = SelectQueryField("Career:", query = Career.select(), get_label = "name", allow_blank = True, blank_text = " ", validators = [Required()])
-
+	# First, let's hardcode some data:
 	income_levels = ["0-30,000", "30,001-48,000", "48,001-75,000", "75,001-110,000", "over 110,000"]
-	income_choices = [(None, "")]
+	income_choices = [("", "")]
 	for level in income_levels:
 		if level == "over 110,000":
 			income_choices.append((level, "Over $110,000"))
 		else:
 			income_choices.append((level, "$"+level))
-	income = SelectField("What's your family's income level?", choices = income_choices)
 
-	budget = StringField("What's your budget for higher education? $")
+	states = ["Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"]
+	state_choices = [("", "")]
+	for state in states:
+		state_choices.append((state, state))
 
-	city = StringField("What city do you live in?")
-	state = StringField("What state do you live in?")
+	# Now let's generate some fields!
+	first_name = StringField("First name:", validators = [Required("Please enter your first name.")])
+	last_name = StringField("Last name:", validators = [Required("Please enter your last name.")])
+	email = StringField("Email:", validators = [Required("Please enter your email."), Email("Please enter an email address.")])
+	career = SelectQueryField("Career:", query = Career.select(), get_label = "name", allow_blank = True, blank_text = " ", validators = [Required("Please choose a career.")])
+	income = SelectField("What's your family's income level?", choices = income_choices, validators = [Required("Please select your family's income level.")])
+	budget = StringField("What's your budget for higher education? $", validators = [Required("Please enter a budget.")])
+	city = StringField("What city do you live in?", validators = [Required("Please enter the city you live in.")])
+	state = SelectField("What state do you live in?", choices = state_choices, validators = [Required("Please enter the state you live in.")])
 
 
 
@@ -124,10 +121,10 @@ def questions():
 		except IntegrityError:
 			flash("It looks like you've already created a profile. Try logging in!")
 			return redirect("/questions")
-	
-	if len(form.errors) > 0:
-		for error in form.errors:
-			flash(error)
+	else:
+		for field, errors in form.errors.items():
+			for error in errors:
+				flash(error)
 
 	return render_template("questions.html", form = form)
 
